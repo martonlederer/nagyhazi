21cd7e97288a53d5b41e73cbda49a0b90e4f1eb4
Marton Lederer <marton@lederer.hu>
2023-11-01 14:48:14 +0100

    chore: skeleton

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    printf("You have entered %d arguments:\n", argc);
+
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+
+    // TODO: init program setup
+    // request initial data:
+    // - at least one table with its data
+    // - at least one item on the menu
+    // after that
+    // - save given data (by default all tables are unoccupied)
+
+    // TODO: switch/case for cmd arguments
+
     return 0;
 }
