2c9175b0aaec313283bd689e80a4f299f9cc6da7
Marton Lederer <marton@lederer.hu>
2023-11-26 22:35:10 +0100

    feat: finish gui

--- gui.c
+++ gui.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "io.h"
 #include "gui.h"
+#include "order.h"
+#include "table.h"
 
 /**
  * Választható menüpontok
@@ -78,14 +80,114 @@ GuiHandleResult handleMenu(
     // választott menü kiírása
     printf(REQUEST "%s:\n" RESET, menus[selected - 1]);
 
+    // a különböző esetekhez szükséges a kapcsos zárójelekkel
+    // való blokkokra bontás, hogy újra lehessen deklarálni
+    // az adott változókat ugyan azzal a névvel
     switch (selected) {
-        case 1:
+        case 1: {
+            // beolvassuk az asztal számát
             printf("Az asztal száma: ");
             int tableIndex;
             scanf("%d", &tableIndex);
-            tableList = openTable(tableIndex - 1, tableList);
 
+            // asztal megnyitása
+            tableList = openTable(tableIndex - 1, tableList);
+        } break;
+
+        case 2: {
+            // menü elem és asztal számának beolvasása
+            printf("Az étel menün szereplő száma: ");
+            int menuIndex;
+            scanf("%d", &menuIndex);
+
+            printf("Az asztal száma: ");
+            int tableIndex;
+            scanf("%d", &tableIndex);
+
+            // új rendelés
+            orderList = orderNewItem(
+                menuIndex,
+                tableIndex,
+                menuList,
+                tableList,
+                orderList
+            );
+        } break;
+
+        case 3: {
+            // asztal számának beolvasása
+            printf("Az asztal száma: ");
+            int tableIndex;
+            scanf("%d", &tableIndex);
+
+            // számlázás
+            BillResult billRes = issueBill(
+                tableIndex,
+                menuList,
+                tableList,
+                orderList
+            );
+
+            // frissítjük a listákat
+            tableList = billRes.tableList;
+            orderList = billRes.orderList;
+        } break;
+
+        case 4:
+            // asztal térkép nyomtatása
+            printTableMap(tableList);
             break;
+
+        case 5:
+            // menü megjelenítése
+            printMenu(menuList);
+            break;
+
+        case 6: {
+            // beolvassuk a nevet és az árat
+            printf("Új étel neve: ");
+            char *name = readStringLine();
+
+            printf("Új étel ára: ");
+            int price;
+            scanf("%d", &price);
+
+            // hozzáadás a menü listához
+            menuList = newMenuItem(
+                name,
+                price,
+                menuList
+            );
+        } break;
+
+        case 7: {
+            // beolvassuk a menü elem indexét
+            printf("Étel száma a menün: ");
+            int index;
+            scanf("%d", &index);
+
+            // kiszedjük a menü listából
+            menuList = removeMenuItem(index, menuList);
+        } break;
+
+        case 8: {
+            // beolvassuk az új asztal adatait
+            printf("Elhelyezkedés (x y): ");
+            int x, y;
+            scanf("%d %d", &x, &y);
+
+            printf("Kapacitás (hány fő fér el): ");
+            int capacity;
+            scanf("%d", &capacity);
+
+            // hozzáadjuk az asztal láncolt listához
+            tableList = newTable(
+                x,
+                y,
+                capacity,
+                tableList
+            );
+        } break;
 
         default:
             printf(ERROR "A menüpont nem található.\n" RESET);
